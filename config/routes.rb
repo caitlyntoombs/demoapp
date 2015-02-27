724a242e1c5f5bd867c406b08eec53cfa4f9deda
CAITLYNTOOMBS <caitlyn.toombs@gmail.com>
2015-02-27 03:58:17 +0000

    made tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  resources :tweets
 
-  get 'static_pages/help'
+  root 'static_pages#home'
+
+  match 'help', to: 'static_pages#help', via: 'get'
 
   resources :users
 
